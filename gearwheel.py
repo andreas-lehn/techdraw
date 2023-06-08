f81cd8e7a57843232f0bd595c8f77f61f66292b3
Andreas Lehn <andreas.lehn@icloud.com>
2023-06-08 08:39:58 +0200

    gearwheel.py runs again.

--- gearwheel.py
+++ gearwheel.py
@@ -44,23 +44,23 @@ class GearWheel:
 
     def beta_head(self):
         '''returns the angle offsets of the tooths head point'''
-        return self.beta_base() - involute.gamma(involute.inverse(self.r_head() / self.r_base()))
+        return self.beta_base() - involute.gamma(involute.inverse(self.r_base(), self.r_head()))
     
     def svg_path(self):
         r_0, r_h, r_b, r_f = self.r_0(), self.r_head(), self.r_base(), self.r_foot()
         b_0, b_h, b_b, b_f = self.beta_0(), self.beta_head(), self.beta_base(), self.theta() / 2
 
-        path = svg.PathCreator(involute.polar2xy(r_f, -b_f), -b_f / 2 + math.pi / 2)
+        path = svg.PathCreator(svg.pol2cart(r_f, -b_f), -b_f / 2 + math.pi / 2)
         for i in range(self.n_teeth):
             offset = i * self.theta()
-            path.bezier_to(involute.polar2xy(r_b, offset - b_b), offset - b_b)
-            path.bezier_to(involute.polar2xy(r_0, offset - b_0), offset - b_0 + self.alpha)
-            path.bezier_to(involute.polar2xy(r_h, offset - b_h), offset - b_h + involute.inverse(r_h / r_b))
-            path.arc_to(involute.polar2xy(r_h, offset + b_h), r_h)
-            path.alpha = offset + b_h - involute.inverse(r_h / r_b)
-            path.bezier_to(involute.polar2xy(r_0, offset + b_0), offset + b_0 - self.alpha)
-            path.bezier_to(involute.polar2xy(r_b, offset + b_b), offset + b_b)
-            path.bezier_to(involute.polar2xy(r_f, offset + b_f), offset + b_f + math.pi / 2)
+            path.bezier_to(svg.pol2cart(r_b, offset - b_b), offset - b_b)
+            path.bezier_to(svg.pol2cart(r_0, offset - b_0), offset - b_0 + self.alpha)
+            path.bezier_to(svg.pol2cart(r_h, offset - b_h), offset - b_h + involute.inverse(r_b, r_h))
+            path.arc_to(svg.pol2cart(r_h, offset + b_h), r_h)
+            path.alpha = offset + b_h - involute.inverse(r_b, r_h)
+            path.bezier_to(svg.pol2cart(r_0, offset + b_0), offset + b_0 - self.alpha)
+            path.bezier_to(svg.pol2cart(r_b, offset + b_b), offset + b_b)
+            path.bezier_to(svg.pol2cart(r_f, offset + b_f), offset + b_f + math.pi / 2)
         path.close()
         return path.d
 
