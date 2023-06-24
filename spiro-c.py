1ccdd341eadd3b33506ad9c1ce4a37faee5ea727
Andreas Lehn <andreas.lehn@icloud.com>
2023-06-24 23:50:18 +0200

    First version of spirograph compiler (spiro-c.py) implemented.

---
+++ spiro-c.py
@@ -0,0 +1,49 @@
+#!/usr/env python3
+
+import argparse
+import svghelper as svg
+import json
+from spirograph import Spirograph
+import sys
+import os
+
+def get_value(data: dict, key: str, default=None):
+    if key in data.keys():
+        result = data[key]
+        del data[key]
+    else:
+        if default == None:
+            raise f'Mandatory value {key} not found.'
+        result = default
+    return result
+
+def parse_hook(data: dict):
+    ring = int(get_value(data, 'ring'))
+    wheel = int(get_value(data, 'wheel'))
+    excenter = get_value(data, 'excenter', 0.8)
+    offset = get_value(data, 'offset', 0)
+    samples = get_value(data, 'samples', 1)
+    return Spirograph(ring, wheel, excenter, offset, samples), data
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Generates an SVG image with a spirograph.')
+    parser.add_argument('filename', type=str, help='file name')
+    args = parser.parse_args()
+    base, ext = os.path.splitext(args.filename)
+    outfile = base + '.svg' if ext == '.spiro' else args.filename + '.svg'
+
+    with open(args.filename) as f:
+        data = json.load(f, object_hook=parse_hook)
+
+    r_max = 0
+    for spirograph, _ in data:
+        if spirograph.r_max() > r_max:
+            r_max = spirograph.r_max()
+
+    width = 2 * r_max
+    img = svg.Image((width, width), (r_max, r_max))
+    img.desc.text = f'Spirograph from file: {args.filename}'
+    for spirograph, attrib in data:
+        svg.Path(img.content, spirograph.svg_path(), { 'stroke-width': '0.5', 'stroke': 'black', 'fill': 'none', **attrib })
+    
+    img.write(outfile)
