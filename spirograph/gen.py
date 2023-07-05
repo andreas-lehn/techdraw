1e6b543097dc77f334b60bea4e7d1f83e318cdd1
Andreas Lehn <andreas.lehn@icloud.com>
2023-07-05 21:06:59 +0200

    Docu improved.

--- spirograph/gen.py
+++ spirograph/gen.py
@@ -1,3 +1,13 @@
+'''Generate a spirograph
+
+This tool generates an SVG file with a spirograph.
+The parameters of the spirograph are determined by the command line parameters.
+
+Use:
+    python -m spirograph.gen --help
+
+for details about the parameters.
+'''
 
 import argparse
 import svghelper as svg
@@ -5,34 +15,35 @@ import sys
 from spirograph import Spirograph
 
 if __name__ == "__main__":
-    prog = 'spirograph.gen'
-    description = 'Command line tool to generate spirographs as SVG files'
-    parser = argparse.ArgumentParser(prog=prog, description=description)
+    PROG = 'python3 -m spirograph.gen'
+    DESCRIPTION = 'Generate spirographs SVG files.'
+    parser = argparse.ArgumentParser(prog=PROG, description=DESCRIPTION)
+    #parser = argparse.ArgumentParser(description=DESCRIPTION)
     parser.add_argument('filename', type=str, help='name of output SVG file', nargs='?', default=sys.stdout)
     parser.add_argument('-r', '--ring', type=int, help='number of teeth of the ring', default=105)
     parser.add_argument('-w', '--wheel', type=int, help='number of teeth of the wheel', default=50)
-    parser.add_argument('-e', '--excenter', type=float, help='excenter', default=0.8)
-    parser.add_argument('-o', '--offset', type=int, help='offset of rotator', default=0)
-    parser.add_argument('-s', '--samples', type=int, help='samples per teeth step.', default=1)
+    parser.add_argument('-e', '--excenter', type=float, help='excenter value of the pen', default=0.8)
+    parser.add_argument('-o', '--offset', type=int, help='offset of the wheel at its start position', default=0)
+    parser.add_argument('-s', '--samples', type=int, help='samples per tooth step', default=1)
     args = parser.parse_args()
 
     args.ring = abs(args.ring)
     args.excenter = abs(args.excenter)
 
     if args.ring <= args.wheel:
-        print(f'{prog}: Wheel must be smaller than ring! No spriograph generated.', file=sys.stderr)
+        print(f'{PROG}: Wheel must be smaller than ring! No spriograph generated.', file=sys.stderr)
         sys.exit(-1)
 
     if args.wheel == 0:
-        print(f'{prog}: Wheel must not be zero! No spriograph generated.', file=sys.stderr)
+        print(f'{PROG}: Wheel must not be zero! No spriograph generated.', file=sys.stderr)
         sys.exit(-1)
 
     if args.excenter > 0.9:
         args.excenter = 0.9
-        print(f'{prog}: Excenter limited to {args.excenter}', file=sys.stderr)
+        print(f'{PROG}: Excenter limited to {args.excenter}', file=sys.stderr)
     
     if args.samples < 1:
-        print(f'{prog}: Number of samples must be > 0 but is {args.samples}. Value set to 1.', file=sys.stderr)
+        print(f'{PROG}: Number of samples must be > 0 but is {args.samples}. Value set to 1.', file=sys.stderr)
         args.samples = 1
 
     spirograph = Spirograph(args.ring, args.wheel, args.excenter, args.offset, args.samples)
